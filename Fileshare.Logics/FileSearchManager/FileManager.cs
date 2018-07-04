be414bfc08091bbc78d77f0e2723e4b2ff2076f8
Tom Branson <tomspharmacy@yahoo.com>
2018-07-04 09:52:15 -0700

    Part 19 of 19 complete, 1 bug remain, not fixed

---
+++ Fileshare.Logics/FileSearchManager/FileManager.cs
@@ -0,0 +1,25 @@
+﻿using Fileshare.Domain.Models;
+using Fileshare.Contracts.FileShare;
+using Fileshare.Domain.FileSearch;
+using System;
+
+namespace Fileshare.Logics.FileShareManager
+{
+    public class FileManager : IFileShareService
+    {
+        public void ForwardResults(FileSearchResultModel result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FilePartModel GetAllFileByte(FileMetaData fileMeta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FilePartModel GetFilePartBytes(FilePart filePart, FileMetaData fileMeta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
